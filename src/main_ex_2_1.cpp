751b19c19f061818d5ecf091e155873bfccf05e7
Henry Eschenmoser <henry.eschenmoser@gmail.com>
2025-02-17 16:11:01 +0100

    Add project files.

---
+++ src/main_ex_2_1.cpp
@@ -0,0 +1,82 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+#define numVAOs 1
+
+using namespace std;
+
+GLuint renderingProgram;
+GLuint vao[numVAOs];
+
+GLuint createShaderProgram() {
+	const char* vshaderSource =
+		"#version 430	\n"
+		"void main(void) \n"
+		"{ gl_Position = vec4(0.0, 0.0, 0.0, 1.0); }";
+
+	const char* fshaderSource =
+		"#version 430	\n"
+		"out vec4 color; \n"
+		"void main(void) \n"
+		"{ color = vec4(0.0, 0.0, 1.0, 1.0); }";
+
+	GLuint vShader = glCreateShader(GL_VERTEX_SHADER);
+	GLuint fShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+	glShaderSource(vShader, 1, &vshaderSource, NULL);
+	glShaderSource(fShader, 1, &fshaderSource, NULL);
+	glCompileShader(vShader);
+	glCompileShader(fShader);
+
+	GLuint vfProgram = glCreateProgram();
+	glAttachShader(vfProgram, vShader);
+	glAttachShader(vfProgram, fShader);
+	glLinkProgram(vfProgram);
+
+	return vfProgram;
+}
+
+void init(GLFWwindow* window) {
+	renderingProgram = createShaderProgram();
+	glGenVertexArrays(numVAOs, vao);
+	glBindVertexArray(vao[0]);
+}
+
+float x = 0.0f;
+float inc = 0.01f;
+
+void display(GLFWwindow* window, double currentTime) {
+	glClear(GL_DEPTH_BUFFER_BIT); // Bräuchte ich eigentlich nicht
+	glClearColor(0.0, 0.0, 0.0, 1.0);
+	glClear(GL_COLOR_BUFFER_BIT); // Clears the background to black
+
+	glUseProgram(renderingProgram);
+
+	x += inc;
+	if (x > 100.0f) inc = -0.6f;
+	if (x < 10.0f) inc = 0.6f;
+	glPointSize(x);
+	glDrawArrays(GL_POINTS, 0, 1);
+}
+
+int main(void) {
+	if (!glfwInit()) { exit(EXIT_FAILURE); }
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	GLFWwindow* window = glfwCreateWindow(600, 600, "Chapter2 - programm1", NULL, NULL);
+	glfwMakeContextCurrent(window);
+	if (glewInit() != GLEW_OK) { exit(EXIT_FAILURE); }
+	glfwSwapInterval(1);
+
+	init(window);
+
+	while (!glfwWindowShouldClose(window)) {
+		display(window, glfwGetTime());
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	exit(EXIT_SUCCESS);
+}
